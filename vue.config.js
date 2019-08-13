250b5acfb56d0fd0363f3fc5ecf2c6ffc884b5f0
LLOOG <357216043@qq.com>
2019-08-13 14:10:01 +0800

    休整

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   publicPath: './',
   // TODO
-  assetsDir: 'gblcli',
+  assetsDir: 'dist',
   devServer: {
     // 本地开启的服务的端口号
     port: 8000,
     // 本机ip
-    host: "localhost",
+    host: 'localhost',
     // 自动启动浏览器
     // open: true,
     proxy: {
       '/roadMonitor': {
-        target: 'http://118.31.21.83:8085/roadMonitor', //后端地址
+        target: 'http://118.31.21.83:8085/roadMonitor', // 后端地址
         changeOrigin: true,
         ws: true,
         pathRewrite: {
           '^/roadMonitor': ''
         }
       }
-    },
+    }
   },
-  configureWebpack:{
+  configureWebpack: {
     externals: {
       'AMap': 'AMap'
     }
