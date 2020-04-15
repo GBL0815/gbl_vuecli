d70f43d0ecc8fcc7f7ca2946ac1c68e8be9d4cf6
LLOOG <357216043@qq.com>
2020-04-15 15:44:38 +0800

    添加大小写

---
+++ src/views/help/Help.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="help">
+    <HelloWorld msg="gblcli" />
+    <li>{{dateTime}}</li>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'help',
+  components: { HelloWorld },
+  data () {
+    return {
+      dateTime: this.$utils.getNowDate(0)
+    }
+  },
+  created () {
+    this.$net.get('/test').then(res => {
+      if (res !== 'error') {
+        console.log('请求成功')
+      }
+    })
+  },
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
