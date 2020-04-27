9a8df6667323c0743bbea375eeab8dd310e89d28
LLOOG <357216043@qq.com>
2020-04-27 14:45:59 +0800

    框架初步搭建

---
+++ pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+		<text>{{ time }}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				time: 10102010
+			}
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
