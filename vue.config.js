176e486b22115ec2eb60f3935149aec2e2df41a7
LLOOG <357216043@qq.com>
2020-08-06 12:40:37 +0800

    优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,39 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
 module.exports = {
   publicPath: './',
   // assetsDir: 'gblCLi',
+  productionSourceMap: isDevelopment,
+  configureWebpack: config => {
+    if (!isDevelopment) {
+      // 代码压缩
+      config.plugins.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              warnings: false,
+              drop_debugger: true,
+              drop_console: true,
+              pure_funcs: ['console.log']
+            }
+          },
+          sourceMap: false,
+          parallel: true // 使用多进程提高构件速度
+        })
+      )
+    }
+  },
+  chainWebpack: config => {
+    // 图片压缩
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: true })
+      .end()
+  },
   devServer: {
     disableHostCheck: true,
     // 自动启动浏览器
