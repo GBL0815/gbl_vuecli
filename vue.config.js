b5785788e59677b255b36aa38037c54153aa66ba
LLOOG <357216043@qq.com>
2020-08-20 15:47:43 +0800

    优化打包

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,15 @@ module.exports = {
   publicPath: './',
   // assetsDir: 'gblCLi',
   productionSourceMap: isDevelopment,
+  // 压缩图片
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: true })
+      .end()
+  },
   configureWebpack: config => {
     if (!isDevelopment) {
       // 代码压缩
@@ -23,6 +32,40 @@ module.exports = {
           parallel: true // 使用多进程提高构件速度
         })
       )
+    }
+    // 抽离公共文件
+    config.optimization = {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: /node_modules/,
+            name: 'vendor',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100
+          },
+          common: {
+            chunks: 'all',
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: 'common',
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 60
+          },
+          styles: {
+            name: 'styles',
+            test: /\.(sa|sc|c)ss$/,
+            chunks: 'all',
+            enforce: true
+          },
+          runtimeChunk: {
+            name: 'manifest'
+          }
+        }
+      }
     }
   },
   devServer: {
