7251f3b1ac4211cbf8bf9c53257270b95eb60122
LLOOG <357216043@qq.com>
2020-11-02 11:05:01 +0800

    更新依赖

--- src/utils/regexp.js
+++ src/utils/regexp.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   phoneReg: /^1[3456789]\d{9}$/,
   emailREg: /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]$/
