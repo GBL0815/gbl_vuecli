72358b516ab552a649c9f740660b1849c336e53e
beiliang.ge <32394350+GBL0815@users.noreply.github.com>
2021-02-05 14:54:41 +0800

    配置文件更新

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,23 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path')
 
 module.exports = {
-  base: './',
-  port: 8080,
-  // 是否自动在浏览器打开
-  open: true,
-  // 是否开启 https
-  https: false,
-  // 服务端渲染
-  ssr: false,
   alias: {
     '/@/': path.resolve(__dirname, './src')
   },
+  base: './',
   plugins: [vue()],
-  proxy: {
-    '/api': {
-      target: 'https://mock.yonyoucloud.com/mock/5859/test',
-      changeOrigin: true,
-      ws: true,
-      rewrite: path => path.replace(/^\/api/, '')
+  server: {
+    port: 8080,
+    open: true,
+    https: false,
+    ssr: false,
+    proxy: {
+      '/api': {
+        target: 'https://mock.yonyoucloud.com/mock/5859/test',
+        changeOrigin: true,
+        ws: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
     }
   }
 }
