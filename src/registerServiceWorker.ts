c949e3c4de04f2cea8f2c8a3f3fb2ff91441c0d9
beiliang.ge <32394350+GBL0815@users.noreply.github.com>
2021-04-01 10:54:08 +0800

    规则添加

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,25 +2,25 @@ import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
+    ready () {
       console.log('准备完成')
     },
-    registered() {
+    registered () {
       console.log('注册成功')
     },
-    cached() {
+    cached () {
       console.log('缓存')
     },
-    updatefound() {
+    updatefound () {
       console.log('正在下载新内容...')
     },
-    updated() {
+    updated () {
       console.log('已下载新内容，请刷新！')
     },
-    offline() {
+    offline () {
       console.log('找不到Internet连接,应用程序正在脱机模式下运行')
     },
-    error(error) {
+    error (error) {
       console.error('服务工作者注册过程中出错:', error)
     }
   })
