66e36f625a77c6079c3e9071e18283bae45655ce
beiliang.ge <32394350+GBL0815@users.noreply.github.com>
2021-04-06 09:49:51 +0800

    update

--- src/util/index.js
+++ src/util/index.js
@@ -1,16 +1,19 @@
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
+
 import { useStore } from 'vuex'
 
 import regexp from './regexp'
 import utilFun from './utils'
 
 const utils = () => {
+  const route = useRoute()
   const router = useRouter()
   const store = useStore()
   const regexps = regexp
   const util = utilFun
 
   return {
+    route,
     router,
     store,
     regexps,
