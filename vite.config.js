565d5335ddca4454a863a1f4fb05bf165610a941
beiliang.ge <32394350+GBL0815@users.noreply.github.com>
2021-11-15 11:00:08 +0800

    依赖更新

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path')
 
 module.exports = {
-  alias: {
-    '/@/': path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   base: './',
   plugins: [vue()],
   server: {
     port: 8080,
     open: true,
+    cors: true,
     https: false,
     ssr: false,
     proxy: {
